2296e81a8d75b9fec553a3f5f525114e62f67f31
Ayush Pandey <Ayush Pandey>
2017-01-29 03:13:13 +0530

    sorted the code out

--- public/app.js
+++ public/app.js
@@ -2,12 +2,12 @@ var app = angular.module('socialCops', []);
 
 app.controller('MainCtrl', function($scope,$http) {
   $scope.name = 'World';
-  var txtFile = new XMLHttpRequest();
-  txtFile.open("GET", "./sachin.csv", true);
-  txtFile.onreadystatechange = function()
-    {
-        allText = txtFile.responseText;
-        console.log(alltext);
-        console.log("Hello");
-    };
+  $http({
+  method: 'GET',
+  url: './sachin.csv'
+  }).then(function successCallback(response) {
+    console.log(response);
+  }, function errorCallback(error) {
+    console.log(error);
+  });
 });
